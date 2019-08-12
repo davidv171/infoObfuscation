1e3c4a1d1081f2bbeff5c3374c94b60ad88e251c
davidv171 <david.vuckovic7@gmail.com>
2019-08-12 19:21:31 +0200

    Generate 8x8 blocks fix indexing a bit

--- bitmapalg/eightxeight.go
+++ bitmapalg/eightxeight.go
@@ -2,39 +2,49 @@ package bitmapalg
 
 import "fmt"
 
-//An 8x8 block
-type (
-	Block = [][]uint32
-)
-type (
-	Blocks [][]Block
-)
-
 /*
    Turn bitmap into 8x8 blocks
 */
-func EightxEight(pixels [][]float32) Blocks {
+func EightxEight(pixels [][]float32) [][][][]float32 {
 	//Count how many 8x8 blocks there can be in an x times y matrix
-	x := len(pixels)
-	y := len(pixels[0])
-	fmt.Println("Pixels: ", len(pixels), " x " , len(pixels[0]))
-	blocks := createBlocks(x, y)
+	length := len(pixels)
+	width := len(pixels[0])
+	fmt.Println("Pixels: ", len(pixels), " x ", len(pixels[0]))
+	blocks := createBlocks(length, width)
+	f := 0
+	s := 0
 	for i := range (blocks) {
 		for j := range (blocks[i]) {
-			for z := j/8; z < 8; z++ {
-				for y := z/8; y < 8;y++ {
-					blocks[i][j][z][y] = pixels[z][y]
-					fmt.Print("[",z,",",y, "]")
+			//Check for right corner
+			for z := s * 8; z/8 < s+1; z++ {
+				fmt.Print("|")
+				for y := f * 8; y/8 < f+1; y++ {
+					fmt.Print("[", z, ",", y, "]")
+					b := z%8
+					n := y%8
+					blocks[i][j][b][n] = pixels[z][y]
 				}
-				fmt.Println()
+				fmt.Println("|")
 			}
-			fmt.Println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+			f++
+			if f == length/8 {
+				f = 0
+				s++
+			}
+			fmt.Println("f", f)
 		}
+		if s == width/8 {
+			s = 0;
+			f++
+		}
+
 		fmt.Println("--------------------------------------------------------------------------------")
+		fmt.Println("")
+
 	}
 
 	fmt.Println(blocks)
-	return Blocks{}
+	return blocks
 }
 
 func createBlocks(x int, y int) [][][][]float32 {
